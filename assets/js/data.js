c993318975e06dde8ad9e941aab92c787de18fc0
Clickadelic <toby.hopp@gmail.com>
2022-10-06 23:50:00 +0200

    Sidebar finished

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,9 +3,10 @@ import data from '../../data/recipes.json' assert { type: 'json' };
 function renderData(){
     let allCards = '';
     const gridContainer = document.getElementById('the-recipe-grid');
-    data.forEach( recipe => {
+    data.forEach( (recipe) => {
         let recipeCard = '<div class="grid-item">';
-        recipeCard += '<img src="./static/images/small/'+recipe.imagename+'" class="img-recipe" alt="Alttext" />';
+        recipeCard += '<div class="overlay"><span>'+recipe.rating+'</span></div>';
+        recipeCard += '<img src="./static/images/small/'+recipe.imagename+'" class="img-recipe" alt="'+recipe.title+'" />';
         recipeCard += '<h3>'+recipe.title+'</h3>';
         recipeCard += '</div>';
         allCards += recipeCard;
---
+++ assets/js/slideshow.js
@@ -0,0 +1,8 @@
+const slides = [
+    'dish-with-potatoes.jpg',
+    'love-is-the-best-ingridient.jpg',
+    'meat-and-corn.jpg',
+    'strawberrys-and-cookies.jpg'
+]
+
+const slider = document.getElementById('')
