01cca172913e22ebbc9e01ea9fa56920961f0b41
Clickadelic <toby.hopp@gmail.com>
2022-10-06 22:39:14 +0200

    restructure

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,4 +1,4 @@
-import data from '../../data/recipes.json' assert {type: 'json'};
+import data from '../../data/recipes.json' assert { type: 'json' };
 
 function renderData(){
     let allCards = '';
