5a0aed44ca4254f051020ef05e346fa9fd64b60b
Clickadelic <toby.hopp@gmail.com>
2022-10-07 02:02:46 +0200

    Ingredients Interface

--- assets/js/data.js
+++ assets/js/data.js
@@ -5,7 +5,7 @@ function renderData(){
     const gridContainer = document.getElementById('the-recipe-grid');
     data.forEach( (recipe) => {
         let recipeCard = '<div class="grid-item">';
-        recipeCard += '<div class="overlay"><span>'+recipe.rating+'</span></div>';
+        recipeCard += '<div class="card-overlay"><span>'+recipe.rating+'</span></div>';
         recipeCard += '<img src="./static/images/small/'+recipe.imagename+'" class="img-recipe" alt="'+recipe.title+'" />';
         recipeCard += '<h3 class="oswald">'+recipe.title+'</h3>';
         recipeCard += '</div>';
